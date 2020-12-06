4219f94303cf395a46271a133c9b512e73a2180f
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-12-06 15:42:20 -0500

    makes presets responsive

--- src/components/Simulations.jsx
+++ src/components/Simulations.jsx
@@ -17,17 +17,35 @@ const Simulations = ({ presets, loadPreset, deletePreset }) => {
                     {presets.map((preset, index) => {
                         return (
                             <div className="preset" key={index}>
-                                <p>
-                                    W: <span className="preset-value">{preset.weight}</span>
-                                    T: <span className="preset-value">{preset.thrust}</span>
-                                    ΔT:{" "}
-                                    <span className="preset-value">{preset.thrustDuration}</span>
-                                    v: <span className="preset-value">{preset.wind}</span>
-                                    FL: <span className="preset-value">{preset.length}</span>
-                                    FD: <span className="preset-value">{preset.diameter}</span>
-                                    FS: <span className="preset-value">{preset.finSpan}</span>
-                                    FC: <span className="preset-value">{preset.finChord}</span>
-                                </p>
+                                <div>
+                                    <div className="preset-value-container">
+                                        W: <span className="preset-value">{preset.weight}</span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        T: <span className="preset-value">{preset.thrust}</span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        ΔT:{" "}
+                                        <span className="preset-value">
+                                            {preset.thrustDuration}
+                                        </span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        v: <span className="preset-value">{preset.wind}</span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        FL: <span className="preset-value">{preset.length}</span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        FD: <span className="preset-value">{preset.diameter}</span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        FS: <span className="preset-value">{preset.finSpan}</span>
+                                    </div>
+                                    <div className="preset-value-container">
+                                        FC: <span className="preset-value">{preset.finChord}</span>
+                                    </div>
+                                </div>
                                 <div className="button-container">
                                     <button className="danger" onClick={() => deletePreset(index)}>
                                         Delete
